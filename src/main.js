665026390ca80ddaa699315ccdd2b18e63765f87
Mplan <mplan_@outlook.com>
2023-08-26 13:39:09 +0800

    对条形统计图进行修整

--- src/main.js
+++ src/main.js
@@ -27,9 +27,30 @@ window.addEventListener("DOMContentLoaded", () => {
 //网页启动时自动运行程序
 window.onload = onload;
 
+
 // 条形统计图代码
 const ctx = document.getElementById("students_chart");
 
+var chart=new Chart(ctx, {
+  type: "bar",
+  data: {
+    labels: [],
+    datasets: [
+      {
+        label: "抽取次数",
+        data: [],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
 //条形统计图代码
 
 async function renew() {
@@ -40,47 +61,29 @@ async function renew() {
   // list_4.textContent = await invoke("get_list",{index:4});
   // list_5.textContent = await invoke("get_list",{index:5});
   //更新条形统计图信息'
-  let s1 = await invoke("get_name",{index:1});
-  let s2 = await invoke("get_name",{index:2});
-  let s3 = await invoke("get_name",{index:3});
-  let s4 = await invoke("get_name",{index:4});
-  let s5 = await invoke("get_name",{index:5});
-  let s6 = await invoke("get_name",{index:6});
-  let s7 = await invoke("get_name",{index:7});
-  let s8 = await invoke("get_name",{index:8});
-  let s9 = await invoke("get_name",{index:9});
-  let s10 = await invoke("get_name",{index:10});
-  let c1 = await invoke("get_count",{index:1});
-  let c2 = await invoke("get_count",{index:2});
-  let c3 = await invoke("get_count",{index:3});
-  let c4 = await invoke("get_count",{index:4});
-  let c5 = await invoke("get_count",{index:5});
-  let c6 = await invoke("get_count",{index:6});
-  let c7 = await invoke("get_count",{index:7});
-  let c8 = await invoke("get_count",{index:8});
-  let c9 = await invoke("get_count",{index:9});
-  let c10 = await invoke("get_count",{index:10});
-
-  new Chart(ctx, {
-    type: "bar",
-    data: {
-      labels: [s1, s2, s3, s4, s5, s6,s7,s8,s9,s10],
-      datasets: [
-        {
-          label: "抽取次数",
-          data: [c1, c2, c3, c4, c5, c6,c7,c8,c9,c10],
-          borderWidth: 1,
-        },
-      ],
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true,
-        },
-      },
-    },
-  });
+  let s1 = await invoke("get_name", { index: 1 });
+  let s2 = await invoke("get_name", { index: 2 });
+  let s3 = await invoke("get_name", { index: 3 });
+  let s4 = await invoke("get_name", { index: 4 });
+  let s5 = await invoke("get_name", { index: 5 });
+  let s6 = await invoke("get_name", { index: 6 });
+  let s7 = await invoke("get_name", { index: 7 });
+  let s8 = await invoke("get_name", { index: 8 });
+  let s9 = await invoke("get_name", { index: 9 });
+  let s10 = await invoke("get_name", { index: 10 });
+  let c1 = await invoke("get_count", { index: 1 });
+  let c2 = await invoke("get_count", { index: 2 });
+  let c3 = await invoke("get_count", { index: 3 });
+  let c4 = await invoke("get_count", { index: 4 });
+  let c5 = await invoke("get_count", { index: 5 });
+  let c6 = await invoke("get_count", { index: 6 });
+  let c7 = await invoke("get_count", { index: 7 });
+  let c8 = await invoke("get_count", { index: 8 });
+  let c9 = await invoke("get_count", { index: 9 });
+  let c10 = await invoke("get_count", { index: 10 });
+  chart.data.datasets[0].data = [c1, c2, c3, c4, c5, c6, c7, c8, c9, c10];
+  chart.data.labels = [s1, s2, s3, s4, s5, s6, s7, s8, s9, s10];
+  chart.update();
 }
 
 async function greet() {
@@ -96,8 +99,6 @@ async function get_random_num() {
 async function onload() {
   renew();
 }
-
-
 
 window.greet = greet;
 window.get_random_num = get_random_num;
